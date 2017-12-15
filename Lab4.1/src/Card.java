f3b9116e612e4f26035a25708298a0dc7451332d
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-12-15 13:06:15 -0500

    Card

--- Lab4.1/src/Card.java
+++ Lab4.1/src/Card.java
@@ -1,8 +1,36 @@
 
 public class Card {
-
-	public Card() {
-		// TODO Auto-generated constructor stub
+	public String rank;
+	public String suit;
+	public int pointValue;
+	
+	public Card(String rank, String suit, int pointValue) {
+	 this.rank = rank;
+	 this.suit = suit;
+	 this.pointValue = pointValue;
 	}
-
+	public String getRank()
+	{
+		return this.rank;
+	}
+	public String getSuit()
+	{
+		return this.suit;
+	}
+	public int getPointValue()
+	{
+		return this.pointValue;
+	}
+	public boolean equals(Card otherCard)
+	{
+		if(this.rank == otherCard.getRank() && this.suit == otherCard.getSuit() && this.pointValue == otherCard.getPointValue())
+		{
+			return true;
+		}
+		return false;
+	}
+	public String toString()
+	{
+		return this.rank+ this.suit+ this.pointValue;
+	}
 }
